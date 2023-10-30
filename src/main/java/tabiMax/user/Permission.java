3b3d624bceb2c3478641cc7055dcc51d9348c4f6
Kiethehe <kietcon12347@gmail.com>
2023-10-30 18:39:53 +0700

    update permission

--- src/main/java/tabiMax/user/Permission.java
+++ src/main/java/tabiMax/user/Permission.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public enum Permission {
+enum Permission {
 
     ADMIN_READ("admin:read"),
     ADMIN_UPDATE("admin:update"),
